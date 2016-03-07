0d2333e7fde09d39c12812d5082763373e2cabb3
broncho <1@broncho.local>
2016-03-07 09:20:19 +0800

    Initial Commit

---
+++ ZY601YiChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZY601YiChat
+//
+//  Created by broncho on 16/3/7.
+//  Copyright © 2016年 xiabeibei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZY601YiChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZY601YiChat
+//
+//  Created by broncho on 16/3/7.
+//  Copyright © 2016年 xiabeibei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
